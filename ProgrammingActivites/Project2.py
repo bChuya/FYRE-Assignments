0f3e80f6915f4582fdc810637a5af6cb27042387
Bryan Marcelo Chuya <bmc330@lehigh.edu>
2026-09-09 15:12:13 -0400

    Clean up comments and modify print output
    Removed redundant comments and updated print statement.

--- ProgrammingActivites/Project2.py
+++ ProgrammingActivites/Project2.py
@@ -1,3 +1,17 @@
 # This program was created in Arduino Lab for MicroPython
+# This program was created in Arduino Lab for MicroPyth
+print("\n")
+print( "Bryan Chuya ")
+# This program was created in Arduino Lab for MicroPython
+# This program was created in Arduino Lab for MicroPython
+# This program was created in Arduino Lab for MicroPython
+# This program was created in Arduino Lab for MicroPython
+# This program was created in Arduino Lab for MicroPython
+# This program was created in Arduino Lab for MicroPython
+# This program was created in Arduino Lab for MicroPython
+# This program was created in Arduino Lab for MicroPython
+# This program was created in Arduino Lab for MicroPython
+# This program was created in Arduino Lab for MicroPython
+# This program was created in Arduino Lab for MicroPython
+# This program was created in Arduino Lab for MicroPython
 
-print('Hello, MicroPython!')
