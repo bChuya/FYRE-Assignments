81d94a0972672f0c9b70860a64c085dd3995d05c
Bryan Marcelo Chuya <bmc330@lehigh.edu>
2026-09-09 15:12:35 -0400

    Create Project3.py for Arduino Lab
    Add a new Python script for MicroPython project.

---
+++ ProgrammingActivites/Project3.py
@@ -0,0 +1,17 @@
+# This program was created in Arduino Lab for MicroPython
+# This program was created in Arduino Lab for MicroPython
+name = "Bryan Chuya "
+print("\n")
+print(name)
+# This program was created in Arduino Lab for MicroPython
+# This program was created in Arduino Lab for MicroPython
+# This program was created in Arduino Lab for MicroPython
+# This program was created in Arduino Lab for MicroPython
+# This program was created in Arduino Lab for MicroPython
+# This program was created in Arduino Lab for MicroPython
+# This program was created in Arduino Lab for MicroPython
+# This program was created in Arduino Lab for MicroPython
+# This program was created in Arduino Lab for MicroPython
+# This program was created in Arduino Lab for MicroPython
+# This program was created in Arduino Lab for MicroPython
+# This program was created in Arduino Lab for MicroPython
